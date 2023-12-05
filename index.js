1233f998cd1711d70e1cbe6cf2451e43460f577b
Paulo Jesus <131003750+PauloJesus45@users.noreply.github.com>
2023-12-05 18:35:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const url = (request, response, next) => {
     next()
 }
 
-app.post('/order', url, (request, response) => {
+app.post("/order", url, (request, response) => {
     const { order, clientName, price, status } = request.body
 
     const o = { id: v4(), order, clientName, price, status }
@@ -39,11 +39,11 @@ app.post('/order', url, (request, response) => {
     return response.status(201).json(orders)
 })
 
-app.get('/order', url, (request, response) => {
+app.get("/order", url, (request, response) => {
     return response.json(orders)
 })
 
-app.put('/orders/:id', checkId, url, (request, response) => {
+app.put("/orders/:id", checkId, url, (request, response) => {
     const { order, clientName, price, status } = request.body
     const id = request.orderId
     const index = request.orderIndex
@@ -55,7 +55,7 @@ app.put('/orders/:id', checkId, url, (request, response) => {
     return response.status(201).json(updateOrder)
 })
 
-app.delete('/order/:id', checkId, (request, response) => {
+app.delete("/order/:id", checkId, (request, response) => {
     const index = request.orderIndex
 
     orders.splice(index, 1)
@@ -63,17 +63,17 @@ app.delete('/order/:id', checkId, (request, response) => {
     return response.status(204).json()
 })
 
-app.get('/order/:id', checkId, url, (request, response) => {
+app.get("/order/:id", checkId, url, (request, response) => {
     const { id } = request.params
     const index = orders.findIndex(order => order.id === id)
 
     return response.json(orders[index])
 })
 
-app.patch('/order/:id', checkId, url, (request, response) => {
+app.patch("/order/:id", checkId, url, (request, response) => {
     const { id } = request.params
     const index = orders.findIndex(order => order.id === id)
-    orders[index].status = 'Pronto'
+    orders[index].status = "Pronto"
 
     return response.json(orders[index])
 })
